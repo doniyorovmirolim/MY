967abc3c48fdccccf6840180b4ba3f4eae2b5509
doniyorovmirolim <73066251+doniyorovmirolim@users.noreply.github.com>
2020-10-18 18:46:21 +0500

    js1

---
+++ script.js
@@ -0,0 +1,22 @@
+var name = prompt('Введите своё имя');
+console.log('Меня зовут ' + name);
+
+var age = +prompt('Введите ваш возраст');
+console.log('Мне ' + age);
+
+var task1 = +prompt('Решите пример 25+63=?');
+console.log('Пример 1: 25+63=88 Ваш ответ ' + task1);
+
+var task2 = +prompt('Решите пример 26-18=?');
+console.log('Пример 2: 26-18=8 Ваш ответ ' + task2);
+
+var task3 = +prompt('Решите пример 32*2=?');
+console.log('Пример 3: 32*2=64 Ваш ответ ' + task3);
+
+var task4 = +prompt('Решите пример 144/12= ?');
+console.log('Пример 4: 144/12=12 Ваш ответ ' + task4);
+
+var task5 = +prompt('Решите пример 13%4=?');
+console.log('Пример 5: 13%4=1 Ваш ответ ' + task5);
+
+alert('Откройте консоль для просмотра')
